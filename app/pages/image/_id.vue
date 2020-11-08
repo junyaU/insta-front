2b8f3eb5c7a6e8c8a18295f6665541aabb9a80a9
junyaUch <junya19991215@icloud.com>
2020-11-08 15:17:42 +0900

    プロフィール画像アップロードのフロント実装

---
+++ app/pages/image/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <AppHeader></AppHeader>
+    <h1>プロフィール画像設定</h1>
+    <form action="/api/upload" method="post" enctype="multipart/form-data">
+      <input type="file" name="Image">
+      <input type="hidden" name="userId" :value="$route.params.id">
+      <SubmitButton></SubmitButton>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- app/pages/mypage/_id.vue
+++ app/pages/mypage/_id.vue
@@ -3,6 +3,13 @@
     <AppHeader></AppHeader>
     <div class="profile-area">
       <div class="photo-wrapper">
+        <div id="photo-image">
+          <img v-if="imageData" :src="imageData"  class="image-data">
+          <p v-else>画像未設定</p>
+        </div>
+        <nuxt-link :to="{name: 'image-id', params: {id: data.Id}}" v-if="data.Id == sessionData.userid">
+          <p>プロフィール画像を設定</p>
+        </nuxt-link>
       </div>
       <div class="name-wrapper">
         <h2>{{data.Name}}</h2>
@@ -25,8 +32,15 @@ export default {
   async asyncData({app, params}){
     const paramId = params.id
     const data = await app.$axios.$get(`/api/user/${paramId}`)
-    console.log(data)
-    return {data}
+    const profileImageData = await app.$axios.$get(`/api/getprofileimage/${paramId}`)
+    const sessionData = await app.$axios.$get(`/api/getsession`)
+    let imageData = ""
+
+    //画像データがあれば読み込む
+    if(profileImageData.image){
+      imageData = 'data:image/jpg;base64,' + profileImageData.image;
+    }
+    return {data, imageData,sessionData}
   }
 }
 </script>
@@ -39,7 +53,8 @@ export default {
     align-items: center;
     padding: 20px 0;
   }
-  .photo-wrapper{
+
+  #photo-image{
     border: 1px solid black;
     height: 200px;
     width: 200px;
@@ -55,5 +70,10 @@ export default {
     margin: 0 ;
     width: 480px ;
     height:400px;
+  }
+
+  .image-data{
+    width: 100%;
+    height: 100%;
   }
 </style>
